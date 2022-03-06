b64e9d832f290599d1d43a70b92ab2f060ab052e
Buff4eyes <claudemokbel@gmail.com>
2022-03-05 19:01:42 -0800

    nav bar position correctly

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -2,21 +2,34 @@ import { render } from '@testing-library/react';
 import React from 'react';
 
 const Navigation = () => {
- return (
-   <nav>
-     <ul>
-       <a href="/"><li>Link 1</li></a>
-       <a href="/"><li>Link 1</li></a>
-       <a href="/"><li>Link 1</li></a>
-     </ul>
-     <p>Branded Message</p>
-     <label htmlFor="lUsername">username:</label>
-      <input type="text" id="lUsername" required></input>
-      <label htmlFor="lPassword">password:</label>
-      <input type="password" id="lPassword" required></input>
-      <button type="submit">login</button>
-   </nav>
- )
+  return (
+    <nav>
+      <div className="left-side">
+          <a className="company-name" href="/">Company Name</a>
+        <div className="links">
+          <ul>
+            <li ><a href="/">Link 1</a></li>
+            <li ><a href="/">Link 2</a></li>
+            <li ><a href="/">Link 3</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="right-side">
+          <span>Branded Message</span>
+        <div id="loginSection">
+          <div id="username">
+            <label htmlFor="lUsername">username:</label>
+            <input type="text" id="lUsername" required></input>
+          </div>
+          <div id="password">
+            <label htmlFor="lPassword">password:</label>
+            <input type="password" id="lPassword" required></input>
+          </div>
+          <button type="submit">login</button>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navigation 
